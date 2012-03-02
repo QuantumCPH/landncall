f0864764667d2240391aa52601da823dbd1bc2fb
Fatima Umer <fu@zerocall.com>
2012-03-02 18:02:03 +0500

    callhistory

--- apps/backend/modules/customer/templates/callhistorySuccess.php
+++ apps/backend/modules/customer/templates/callhistorySuccess.php
@@ -223,7 +223,7 @@ $todate=date("Y-m-d", $tomorrow);
                           $tilentaCallHistryResult = Telienta::callHistory($customer, $fromdate, $todate);
 
 
-                            foreach ($tilentaCallHistryResult->xdr_list as $xdr) { echo "<pre>";echo var_dump($tilentaCallHistryResult);echo "</pre>";
+                            foreach ($tilentaCallHistryResult->xdr_list as $xdr) { //echo "<pre>";echo var_dump($tilentaCallHistryResult);echo "</pre>";
                           //  die;
                             ?>
 
