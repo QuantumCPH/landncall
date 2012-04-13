51a61830012287d7d2efd6acfedf0120145e01db
Rubab Rafique <rr@zerocall.com>
2012-04-13 09:47:29 +0500

    lib files

---
+++ lib/form/ForumTelRequestsForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ForumTelRequests form.
+ *
+ * @package    zapnacrm
+ * @subpackage form
+ * @author     Your name here
+ */
+class ForumTelRequestsForm extends BaseForumTelRequestsForm
+{
+  public function configure()
+  {
+  }
+}
