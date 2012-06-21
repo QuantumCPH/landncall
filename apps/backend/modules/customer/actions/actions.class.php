904a6899c53e554ead0895106f5e1d43d4ea6bf7
Baran Khan <bk@zapna.com>
2012-06-21 16:34:49 +0500

    deActivate e0customer

--- apps/backend/modules/customer/actions/actions.class.php
+++ apps/backend/modules/customer/actions/actions.class.php
@@ -61,7 +61,8 @@ class customerActions extends autocustomerActions {
                 $uniqueIdObj->setStatus(0);
                 $uniqueIdObj->setAssignedAt("0000-00-00 00:00:00");
                 $uniqueIdObj->save();
-
+                $customer->getCustomerStatusId(6);
+                $customer->save();
                 $response_text .= "Customer De-activated, Customer Id=" . $customer_id;
                 $response_text .= '<br/>';
 
