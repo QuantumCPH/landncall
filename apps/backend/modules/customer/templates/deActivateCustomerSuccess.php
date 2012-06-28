30928ffca0c3c01c6990526d266c5d16f65f0796
khanmuhammad <khan.muhammad@zerocall.com>
2012-06-28 15:55:41 +0500

    all changes

--- apps/backend/modules/customer/templates/deActivateCustomerSuccess.php
+++ apps/backend/modules/customer/templates/deActivateCustomerSuccess.php
@@ -12,7 +12,7 @@
     <table border="0">
         <tr><td style="border:none !important;">Customer Id:</td><td style="border:none !important;"><input type="text" name="customer_id" /></td></tr>
           <tr><td style="border:none !important;">Make Unique Id Available Again:</td> <td style="border:none !important;"><input type="checkbox" value="1" name="uniqueId" /></td></tr>
-            <tr><td style="border:none !important;"></td><td> <input type="submit" value="De-Activate"></td></tr>
+            <tr><td style="border:none !important;"></td><td style="border:none !important;"> <input type="submit" value="De-Activate"></td></tr>
 
 
 
