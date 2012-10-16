41d6965c901d407eaa23eab5dd1d7c378ef6ab83
Baran Khan <bk@zapna.com>
2012-10-16 17:57:11 +0500

    invoice fixation

--- lib/model/EmployeeCustomerCallhistoryPeer.php
+++ lib/model/EmployeeCustomerCallhistoryPeer.php
@@ -2,4 +2,25 @@
 
 class EmployeeCustomerCallhistoryPeer extends BaseEmployeeCustomerCallhistoryPeer
 {
+        static public function getTotalCallDuration(Employee $employee,$country_id,PropelPDO $con = null)
+    {
+       if($con === null) {
+          $con = Propel::getConnection(EmployeeCallhistoryPeer::DATABASE_NAME, Propel::CONNECTION_READ);
+       }
+
+       $stmt = $con->prepare('SELECT sec_to_time(sum(' . EmployeeCallhistoryPeer::CHARGED_QUANTITY . ')) FROM ' . EmployeeCallhistoryPeer::TABLE_NAME . ' Where '. EmployeeCallhistoryPeer::EMPLOYEE_ID .'='.$employee->getId().' and '. EmployeeCallhistoryPeer::COUNTRY_ID .'='.$country_id);
+       $stmt->execute();
+       return $stmt->fetchColumn();
+    }
+   
+    static public function getCallDuration($call_id,PropelPDO $con = null)
+    {
+       if($con === null) {
+          $con = Propel::getConnection(EmployeeCallhistoryPeer::DATABASE_NAME, Propel::CONNECTION_READ);
+       }
+
+       $stmt = $con->prepare('SELECT sec_to_time(' . EmployeeCallhistoryPeer::CHARGED_QUANTITY . ') FROM ' . EmployeeCallhistoryPeer::TABLE_NAME . ' Where '. EmployeeCallhistoryPeer::ID .'='.$call_id);
+       $stmt->execute();
+       return $stmt->fetchColumn();
+    }
 }
