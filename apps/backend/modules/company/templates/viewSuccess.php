acaec6d60b116cfc3fbb8dc3e3566fa67810b917
Baran Khan <bk@zapna.com>
2012-10-31 15:03:12 +0500

    fixation

--- apps/backend/modules/company/templates/viewSuccess.php
+++ apps/backend/modules/company/templates/viewSuccess.php
@@ -1,7 +1,7 @@
 <div id="sf_admin_container">
     <div id="sf_admin_content">
         <!-- employee/list?filters[company_id]=1 -->
-        <a href="<?php echo url_for('employee/index') . '?company_id=' . $company->getId() . "&filter=filter" ?>" class="external_link" target="_self">Employees (<?php echo count($company->getEmployees()) ?>)</a>
+        <a href="<?php echo url_for('employee/index') . '?company_id=' . $company->getId() . "&filter=filter" ?>" class="external_link" target="_self">Employees (<?php echo count($employeeCount) ?>)</a>
         <a href="<?php echo url_for('company/usage') . '?company_id=' . $company->getId(); ?>" class="external_link" target="_self">Usage</a>
         <a href="<?php echo url_for('company/paymenthistory') . '?company_id=' . $company->getId() . '&filter=filter' ?>" class="external_link" target="_self">Payment History</a>
         <a href="<?php echo url_for('company/invoices') . '?company_id=' . $company->getId() ?>" class="external_link" target="_self"><?php echo __('Invoices') ?></a>
