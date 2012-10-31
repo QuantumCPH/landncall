03ad9af7b427c3a12816b5cf644de9220cfcf63b
Baran Khan <bk@zapna.com>
2012-10-31 15:32:25 +0500

    fixation

--- apps/backend/modules/employee/actions/actions.class.php
+++ apps/backend/modules/employee/actions/actions.class.php
@@ -35,7 +35,7 @@ class employeeActions extends sfActions {
         $this->companys = CompanyPeer::doSelect($c);
 
         $pr = new Criteria();
-        $pr->add(ProductPeer::ID, 14);
+        $pr->add(ProductPeer::IS_IN_B2B, 1);
         //$pr->add(ProductPeer::IS_IN_ZAPNA, 1);
         $this->products = ProductPeer::doSelect($pr);
     }
