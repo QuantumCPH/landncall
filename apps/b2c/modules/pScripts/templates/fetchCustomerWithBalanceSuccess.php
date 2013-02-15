fe34a8ae1667fcfbc16e17bfafffd8095202f97f
Baran Khan <bk@zapna.com>
2013-02-15 10:48:01 +0500

    fix

--- apps/b2c/modules/pScripts/templates/fetchCustomerWithBalanceSuccess.php
+++ apps/b2c/modules/pScripts/templates/fetchCustomerWithBalanceSuccess.php
@@ -5,6 +5,16 @@
  */
 ?>
 <table >
+     <tr >
+            <th >First Name</th>
+            <th >Last Name</th>
+            <th >Mobile Number</th>
+            <th >Unique Id</th>
+            <th >Created At</th>
+            <th > Registration Type</th>            
+            <th >Balance</th>
+            <th >Agent Company Name</th >
+        </tr>
     <?php
     $i = 0;
     foreach ($customers as $customer) {
@@ -13,11 +23,19 @@
             <td ><?= $customer->getFirstName(); ?></td>
             <td><?= $customer->getLastName(); ?></td>
             <td><?= $customer->getMobileNumber(); ?></td>
+            <td><?= $customer->getUniqueId(); ?></td>
+            <td><?= $customer->getCreatedAt(); ?></td>
+            <td><?= RegistrationTypePeer::retrieveByPK($customer->getRegistrationTypeId())->getDescription(); ?></td>            
             <td><?= Telienta::getBalance($customer); ?></td>
+            <td><? if($customer->getReferrerId()!="")
+                
+                echo AgentCompanyPeer::retrieveByPK($customer->getReferrerId())->getName();
+                
+                ?></td>
         </tr>
 
         <?php
-         
+         break;
     }
     ?>
 </table>
